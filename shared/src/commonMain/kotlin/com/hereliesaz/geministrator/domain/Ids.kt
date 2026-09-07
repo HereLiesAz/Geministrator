1df3479ce2a093651217069bb1959e94e44a1430
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-09-07 14:55:02 -0500

    feat: make domain identifiers serializable

--- shared/src/commonMain/kotlin/com/hereliesaz/geministrator/domain/Ids.kt
+++ shared/src/commonMain/kotlin/com/hereliesaz/geministrator/domain/Ids.kt
@@ -1,13 +1,15 @@
 package com.hereliesaz.geministrator.domain
 
-data class ProjectId(val value: String)
-data class WorkflowDefinitionId(val value: String)
-data class WorkflowRunId(val value: String)
-data class TaskDefinitionId(val value: String)
-data class TaskRunId(val value: String)
-data class RoleDefinitionId(val value: String)
-data class AgentProviderId(val value: String)
-data class ProviderRunId(val value: String)
-data class ArtifactId(val value: String)
-data class WorkflowTemplateId(val value: String)
-data class ApprovalGateId(val value: String)
+import kotlinx.serialization.Serializable
+
+@Serializable data class ProjectId(val value: String)
+@Serializable data class WorkflowDefinitionId(val value: String)
+@Serializable data class WorkflowRunId(val value: String)
+@Serializable data class TaskDefinitionId(val value: String)
+@Serializable data class TaskRunId(val value: String)
+@Serializable data class RoleDefinitionId(val value: String)
+@Serializable data class AgentProviderId(val value: String)
+@Serializable data class ProviderRunId(val value: String)
+@Serializable data class ArtifactId(val value: String)
+@Serializable data class WorkflowTemplateId(val value: String)
+@Serializable data class ApprovalGateId(val value: String)
