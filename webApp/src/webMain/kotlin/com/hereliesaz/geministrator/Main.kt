6c50ad99452ebd6c42c9fe07bf31088d5b80020b
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-09-07 12:52:01 -0500

    feat: add web Compose viewport launcher

---
+++ webApp/src/webMain/kotlin/com/hereliesaz/geministrator/Main.kt
@@ -0,0 +1,11 @@
+package com.hereliesaz.geministrator
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport(viewportContainerId = "webApp") {
+        App()
+    }
+}
