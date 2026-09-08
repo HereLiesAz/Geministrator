ef67ebfbc2c1fb77bac58d6c519b595db2af83a7
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-09-07 21:49:48 -0500

    build(android): support CI release signing

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -16,6 +16,32 @@ android {
         versionName = "2.0.0-alpha01"
     }
 
+    val ciKeystorePath = System.getenv("GEMINISTRATOR_KEYSTORE_PATH")
+    val ciKeystorePassword = System.getenv("KEYSTORE_PASSWORD")
+    val ciKeyAlias = System.getenv("KEY_ALIAS")
+    val ciKeyPassword = System.getenv("KEY_PASSWORD")
+
+    if (
+        !ciKeystorePath.isNullOrBlank() &&
+        !ciKeystorePassword.isNullOrBlank() &&
+        !ciKeyAlias.isNullOrBlank() &&
+        !ciKeyPassword.isNullOrBlank()
+    ) {
+        signingConfigs {
+            create("ciRelease") {
+                storeFile = file(ciKeystorePath)
+                storePassword = ciKeystorePassword
+                keyAlias = ciKeyAlias
+                keyPassword = ciKeyPassword
+            }
+        }
+        buildTypes {
+            getByName("release") {
+                signingConfig = signingConfigs.getByName("ciRelease")
+            }
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
