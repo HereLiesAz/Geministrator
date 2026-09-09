daf1319ac832e57b124fab1456c19ee27b99fe98
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-09-09 09:15:27 -0500

    Reject conflicting workflow task role identities

--- shared/src/commonMain/kotlin/com/hereliesaz/geministrator/workflow/WorkflowGraphValidator.kt
+++ shared/src/commonMain/kotlin/com/hereliesaz/geministrator/workflow/WorkflowGraphValidator.kt
@@ -1,6 +1,8 @@
 package com.hereliesaz.geministrator.workflow
 
+import com.hereliesaz.geministrator.domain.RoleDefinitionId
 import com.hereliesaz.geministrator.domain.TaskDefinitionId
+import com.hereliesaz.geministrator.domain.TaskExecutor
 import com.hereliesaz.geministrator.domain.WorkflowDefinition
 
 sealed interface WorkflowValidationError {
@@ -10,6 +12,11 @@ sealed interface WorkflowValidationError {
         val missingDependencyId: TaskDefinitionId,
     ) : WorkflowValidationError
     data class MissingExecutor(val taskId: TaskDefinitionId) : WorkflowValidationError
+    data class ConflictingRoleIdentity(
+        val taskId: TaskDefinitionId,
+        val responsibleRoleId: RoleDefinitionId,
+        val executorRoleId: RoleDefinitionId,
+    ) : WorkflowValidationError
     data class SelfDependency(val taskId: TaskDefinitionId) : WorkflowValidationError
     data class Cycle(val taskIds: Set<TaskDefinitionId>) : WorkflowValidationError
 }
@@ -26,6 +33,14 @@ object WorkflowGraphValidator {
         definition.tasks.forEach { task ->
             if (task.executor == null && task.roleId == null) {
                 errors += WorkflowValidationError.MissingExecutor(task.id)
+            }
+            val executor = task.executor
+            if (executor is TaskExecutor.RoleAgent && task.roleId != null && task.roleId != executor.roleId) {
+                errors += WorkflowValidationError.ConflictingRoleIdentity(
+                    taskId = task.id,
+                    responsibleRoleId = task.roleId,
+                    executorRoleId = executor.roleId,
+                )
             }
             task.dependsOn.forEach { dependency ->
                 when {
